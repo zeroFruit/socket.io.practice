0a04790f88c49894ec185f874992b8c15d54955b
zerofruit <14wnrkim@gmail.com>
2017-02-01 17:38:34 +0000

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Connected to ${port}`);
+});

0002a4dfb43a39ece0df06e99584378dd4028909
DESKTOP-NUN5RK4\김주형 <14wnrkim@gmail.com>
2017-02-03 16:50:41 +0900

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight      = messages.prop('clientHeight');
+  var scrollTop         = messages.prop('scrollTop');
+  var scrollHeight      = messages.prop('scrollHeight');
+  var newMessageHeight  = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrllTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -18,12 +34,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
-
-
-  // var $li = $('<li></li>');
-  // $li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  //
-  // $('#messages').append($li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -36,6 +47,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
